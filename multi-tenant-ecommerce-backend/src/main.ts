f6ba9e10f42969ff94c68b33fed231b03c14514c
Humanshu Jaglan <humanshujaglan@gmail.com>
2025-11-25 22:05:58 +0530

    done

--- multi-tenant-ecommerce-backend/src/main.ts
+++ multi-tenant-ecommerce-backend/src/main.ts
@@ -10,6 +10,14 @@ async function bootstrap() {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
+    'http://localhost:3005',
+    'http://localhost:3006',
+    'http://localhost:3007',
+    'http://localhost:3008',
+    'http://localhost:3009',
+    'http://localhost:3010',
   ]
 
   app.enableCors({
