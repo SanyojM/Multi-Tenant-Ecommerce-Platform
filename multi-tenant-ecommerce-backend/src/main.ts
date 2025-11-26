575a85b5053f4df103d7a51304894892b037d118
Humanshu Jaglan <humanshujaglan@gmail.com>
2025-11-26 05:54:44 +0000

    fixed cors

--- multi-tenant-ecommerce-backend/src/main.ts
+++ multi-tenant-ecommerce-backend/src/main.ts
@@ -7,27 +7,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const whitelist = [
-    'http://localhost:3000/',
-    'http://localhost:3001/',
-    'http://localhost:3002/',
-    'http://localhost:3003/',
-    'http://localhost:3004/',
-    'http://localhost:3005/',
-    'http://localhost:3006/',
-    'http://localhost:3007/',
-    'http://localhost:3008/',
-    'http://localhost:3009/',
-    'http://localhost:3010/',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
+    'http://localhost:3005',
+    'http://localhost:3006',
+    'http://localhost:3007',
+    'http://localhost:3008',
+    'http://localhost:3009',
+    'http://localhost:3010'
   ]
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
+    origin:whitelist,
+    method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // if using cookies/auth headers
   })
 
